d3e56eb1aa15c2075bb6fb4244c1cc6304ce46b3
Qile0317 <qile0317@gmail.com>
2025-06-27 12:25:13 -0700

    more compiler-token tests

--- tests/testthat/test-compiler-token.R
+++ tests/testthat/test-compiler-token.R
@@ -1,4 +1,36 @@
-test_that("the constructor works", {
+test_that("the EinopsTokenSequence constructor works", {
+
+    expected_object <- structure(
+        list(
+            structure(
+                list(type = "NAME", value = "a", start = 1),
+                class = "EinopsToken"
+            ),
+            structure(
+                list(type = "NAME", value = "b", start = 3),
+                class = "EinopsToken"
+            ),
+            structure(
+                list(type = "NAME", value = "c", start = 5),
+                class = "EinopsToken"
+            ),
+            structure(
+                list(type = "ARROW", value = "->", start = 7),
+                class = "EinopsToken"
+            ),
+            structure(
+                list(type = "NAME", value = "b", start = 10),
+                class = "EinopsToken"
+            ),
+            structure(
+                list(type = "NAME", value = "c", start = 12),
+                class = "EinopsToken"
+            )
+        ),
+        class = c("EinopsTokenSequence", "list")
+    )
+
+    # test a flat sequence
     expect_identical(
         EinopsTokenSequence(
             NameToken("a", 1),
@@ -8,16 +40,62 @@ test_that("the constructor works", {
             NameToken("b", 10),
             NameToken("c", 12)
         ),
-        structure(
-            list(
-                structure(list(type = "NAME", value = "a", start = 1), class = "EinopsToken"),
-                structure(list(type = "NAME", value = "b", start = 3), class = "EinopsToken"),
-                structure(list(type = "NAME", value = "c", start = 5), class = "EinopsToken"),
-                structure(list(type = "ARROW", value = "->", start = 7), class = "EinopsToken"),
-                structure(list(type = "NAME", value = "b", start = 10), class = "EinopsToken"),
-                structure(list(type = "NAME", value = "c", start = 12), class = "EinopsToken")
-            ),
-            class = c("EinopsTokenSequence", "list")
-        )
+        expected_object
     )
+
+    # tokens with token sequences
+
+    expect_identical(
+        EinopsTokenSequence(
+            NameToken("a", 1),
+            NameToken("b", 3),
+            NameToken("c", 5),
+            ArrowToken(7),
+            EinopsTokenSequence(NameToken("b", 10), NameToken("c", 12))
+        ),
+        expected_object
+    )
+
+    expect_identical(
+        EinopsTokenSequence(
+            NameToken("a", 1),
+            EinopsTokenSequence(NameToken("b", 3)),
+            NameToken("c", 5),
+            ArrowToken(7),
+            EinopsTokenSequence(
+                NameToken("b", 10),
+                NameToken("c", 12)
+            )
+        ),
+        expected_object
+    )
+
+    expect_identical(
+        EinopsTokenSequence(EinopsTokenSequence(
+            NameToken("a", 1),
+            NameToken("b", 3),
+            NameToken("c", 5),
+            ArrowToken(7),
+            NameToken("b", 10),
+            NameToken("c", 12)
+        )),
+        expected_object
+    )
+
+    expect_identical(
+        EinopsTokenSequence(
+            NameToken("a", 1),
+            NameToken("b", 3),
+            EinopsTokenSequence(
+                NameToken("c", 5),
+                EinopsTokenSequence(ArrowToken(7))
+            ),
+            EinopsTokenSequence(
+                NameToken("b", 10),
+                NameToken("c", 12)
+            )
+        ),
+        expected_object
+    )
+
 })
