b50e3526d235e3625a12cefec1a3106f150c9ace
Qile0317 <qile0317@gmail.com>
2025-07-04 22:30:22 -0700

    fix c.s3list

--- R/utils-s3list.R
+++ R/utils-s3list.R
@@ -6,7 +6,18 @@ repr.s3list <- function(x, indent = 4L, s3_cons = TRUE, ...) {
     repr.list(x, indent = indent, s3_cons = s3_cons, ...)
 }
 
-# #' @export
-# c.s3list <- function(...) {
-#     structure(c(unlist(lapply(list(...), unclass), recursive = FALSE)), class = class(..1))
-# }
+#' @export
+c.s3list <- function(...) {
+
+    args <- list(...)
+    current_class <- class(..1)
+    
+    output <- lapply(args, function(arg) {
+        processor <- if (inherits(arg, "s3list") && !(any(setdiff(current_class, "s3list") %in% setdiff(class(arg), "s3list")))) list else identity
+        processor(arg)
+    })
+    output <- unlist(list(list(list()), output), recursive = FALSE)
+    output <- do.call(c, output)
+    class(output) <- current_class
+    output
+}
