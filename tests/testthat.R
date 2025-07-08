c5de2c05db44fcb69232b6b2abba99dac589dc71
Qile0317 <qile0317@gmail.com>
2025-07-07 18:17:56 -0700

    make torch auto install deps

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,6 @@
-# This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
-#
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
-# * https://testthat.r-lib.org/articles/special-files.html
-
 library(testthat)
 library(einops)
 
-test_check("einops")
+if (Sys.getenv("TORCH_TEST", unset = 0) == 1) {
+    test_check("einops")
+}
