c805da422e20b7810f55d598f2b6147078617099
Qile0317 <qile0317@gmail.com>
2025-07-12 12:45:23 -0700

    add some failing tests

--- tests/testthat/test-compiler-apply_recipe.R
+++ tests/testthat/test-compiler-apply_recipe.R
@@ -1,6 +1,6 @@
 test_that("create_execution_plan works", {
 
-    # "b c h w -> b h w c" rearrange
+    # rearrange, "b c h w -> b h w c"
 
     recipe <- TransformRecipe(
         elementary_axes_lengths = rep(unknown_axis_length(), 4L),
@@ -21,6 +21,43 @@ test_that("create_execution_plan works", {
             added_axes = r2r::hashmap(),
             final_shapes = integer(),
             n_axes_w_added = 4L
+        )
+    )
+
+    # rearrange, "b (c h1 w1) h w -> b c (h h1) (w w1)", h1 = 2, w1 = 2
+
+    recipe <- TransformRecipe(
+        elementary_axes_lengths = c(
+            rep(unknown_axis_length(), 2L),
+            rep(expected_axis_length(), 2L),
+            rep(unknown_axis_length(), 2L)
+        ),
+        axis_name2elementary_axis = r2r::hashmap(
+            list("h1", 3L), list("w1", 4L)
+        ),
+        input_composition_known_unknown = list(
+            list(known = integer(), unknown = 1L),
+            list(known = c(3L, 4L), unknown = 2L),
+            list(known = integer(), unknown = 5L),
+            list(known = integer(), unknown = 6L)
+        ),
+        axes_permutation = c(1L, 2L, 5L, 3L, 6L, 4L),
+        first_reduced_axis = 7L,
+        added_axes = r2r::hashmap(),
+        output_composite_axes = list(1L, 2L, c(5L, 3L), c(6L, 4L))
+    )
+
+    expect_identical(
+        create_execution_plan(
+            recipe, as.integer(10 * 1:4), list(h1 = 2, w1 = 2)
+        ),
+        EinopsExecutionPlan(
+            init_shapes = integer(10L, 5L, 2L, 2L, 30L, 40L),
+            axes_reordering = c(1L, 2L, 5L, 3L, 6L, 4L),
+            reduced_axes = integer(),
+            added_axes = r2r::hashmap(),
+            final_shapes = integer(10L, 5L, 60L, 80L),
+            n_axes_w_added = 6L
         )
     )
 
--- tests/testthat/test-rearrange.R
+++ tests/testthat/test-rearrange.R
@@ -13,4 +13,52 @@ test_that("rearrange() works", {
         t(x)
     )
 
+    x <- array(1:(10 * 20 * 30 * 40), dim = c(10, 20, 30, 40))
+    
+    # Test 1: transpose
+    y1 <- rearrange(x, "b c h w -> b h w c")
+    expect_equal(dim(y1), c(10, 30, 40, 20))
+    
+    # Test 2: view/reshape
+    y2 <- rearrange(x, "b c h w -> b (c h w)")
+    expect_equal(dim(y2), c(10, 20 * 30 * 40))
+    
+    # Test 3: depth-to-space
+    y3 <- rearrange(x, "b (c h1 w1) h w -> b c (h h1) (w w1)", h1 = 2, w1 = 2)
+    expect_equal(dim(y3), c(10, 5, 30 * 2, 40 * 2))
+    
+    # Test 4: space-to-depth
+    y4 <- rearrange(x, "b c (h h1) (w w1) -> b (h1 w1 c) h w", h1 = 2, w1 = 2)
+    expect_equal(dim(y4), c(10, 20 * 4, 30 %/% 2, 40 %/% 2))
+    
+    # Test 5: simple transposition
+    y5 <- rearrange(x, "b1 sound b2 letter -> b1 b2 sound letter")
+    expect_equal(dim(y5), c(10, 30, 20, 40))
+    
+    # Test 6: parsing parameters
+    t <- rearrange(x, "b c h w -> (b h w) c")
+    t <- t[, seq(1, ncol(t), by = 2)]
+    expect_equal(dim(t), c(10 * 30 * 40, 10))
+    y6 <- rearrange(t, "(b h w) c2 -> b c2 h w", parse_shape(x, "b _ h w"))
+    expect_equal(dim(y6), c(10, 10, 30, 40))
+    
+    # Test 7: split of embedding into groups
+    # Note: R doesn't have tuple unpacking like Python, so we need to handle differently
+    y7_result <- rearrange(x, "b (c g) h w -> g b c h w", g = 2)
+    expect_equal(dim(y7_result), c(2, 10, 10, 30, 40))
+    
+    # Test 10: stack
+    x_list <- list()
+    for (i in 1:10) {
+        x_list[[i]] <- array(i:(i + 20 * 30 * 40 - 1), dim = c(20, 30, 40))
+    }
+    y10 <- rearrange(x_list, "b c h w -> b h w c")
+    expect_equal(dim(y10), c(10, 30, 40, 20))
+    
+    # Test 11: concatenate
+    y11 <- rearrange(x_list, "b c h w -> h (b w) c")
+    expect_equal(dim(y11), c(30, 10 * 40, 20))
+    
+    # TODO rest of tests
+
 })
