f0ad91cb720bde1100eb15e21287a4223318f437
Qile0317 <qile0317@gmail.com>
2025-07-14 22:03:54 -0700

    add indexing for image_tensor

--- R/utils-image_tensor.R
+++ R/utils-image_tensor.R
@@ -12,11 +12,12 @@
 #' learning frameworks.
 #'
 #' @param x An object to convert to or from `image_tensor` format.
+#' @param ... Additional arguments passed to underlying methods. For `[` and
+#'   these are indexing arguments.
 #' @param axes Logical. Whether to show axes in the plot. Default is
 #'   controlled by the option `plot_image_tensor_axes` (default: FALSE).
 #' @param as_image Logical. Whether to print the image as a plot. Default is
 #'   controlled by the option `print_image_tensor_as_plot` (default: TRUE).
-#' @param ... Additional arguments passed to underlying methods.
 #'
 #' @details
 #' The `image_tensor` class provides the following methods:
@@ -29,6 +30,7 @@
 #'   package) to `image_tensor`
 #' - `as.cimg.image_tensor()`: Method to convert `image_tensor` objects back
 #'   to `cimg` format
+#' - `[.image_tensor()`: Subset method for `image_tensor` objects
 #' - `plot.image_tensor()`: Plot method for `image_tensor` objects
 #' - `print.image_tensor()`: Print method for `image_tensor` objects
 #'
@@ -50,6 +52,8 @@
 #' @return
 #' - `as_image_tensor()`: An object of class `image_tensor`
 #' - `as.cimg()`: A `cimg` object (from imager package)
+#' - `[.image_tensor()`: A subset of the `image_tensor` object, also of class
+#'   `image_tensor`
 #' - `plot()`: Invisibly returns the input object
 #' - `print()`: Invisibly returns the input object
 #' @keywords internal
@@ -79,6 +83,27 @@ as.cimg <- function(x) { # nolint: object_name_linter.
 #' @export
 as.cimg.image_tensor <- function(x) {
     imager::as.cimg(rearrange(x, "b h w c -> w h b c"))
+}
+
+#' @rdname image_tensor
+#' @export
+"[.image_tensor" <- function(x, ...) {
+
+    original_class <- class(x)
+    args <- list(...)
+
+    if (length(args) == 1) {
+        return(as_image_tensor(unclass(x)[args[[1]], , , , drop = FALSE]))
+    }
+
+    result <- NextMethod("[", drop = FALSE)
+    result_dims <- dim(result)
+    if (length(result_dims) != 4) {
+        stop("Subsetting resulted in unexpected dimensionality")
+    }
+
+    class(result) <- original_class
+    result
 }
 
 #' @rdname image_tensor
