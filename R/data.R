b5bccce5734df498c3304e350baee0fc6c6ac6c6
Qile0317 <qile0317@gmail.com>
2025-07-29 15:31:39 -0700

    minor doc fixes to pass check

--- R/data.R
+++ R/data.R
@@ -10,7 +10,6 @@
 #' @format An [image_tensor()] object with 6 images, each of size 96 x 96
 #' pixels, with 3 color channels (RGB). The images are stored in a 4D array
 #' with dimensions (6, 96, 96, 3).
-#' @seealso [as.cimg()]
 #' @examples
 #' data("einops_image")
 #' for (i in seq_len(6)) print(einops_image[i, , , ])
