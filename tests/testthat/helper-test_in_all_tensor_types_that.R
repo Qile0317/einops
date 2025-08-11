bf15002ac120937a388243ef27986f909a7a1e28
Qile0317 <qile0317@gmail.com>
2025-08-11 19:09:50 +0000

    fix lints

--- tests/testthat/helper-test_in_all_tensor_types_that.R
+++ tests/testthat/helper-test_in_all_tensor_types_that.R
@@ -56,7 +56,7 @@ test_in_all_tensor_types_that <- function(desc, code) {
 
 #' Interface function for creation of tensors
 #'
-#' This function is a placeholder that should be defined in the test context.
+#' This function is a placeholder that should be defined in the test env.
 #' It is used to create tensors in a backend-agnostic way.
 #' @param values A vector of values to fill the tensor.
 #' @param dims A vector of dimensions for the tensor.
@@ -64,7 +64,7 @@ test_in_all_tensor_types_that <- function(desc, code) {
 #' method.
 #' @return A tensor object created by the backend.
 create_tensor <- function(values, dims, ...) {
-    throw_not_implemented("create_tensor() must be defined in the test context")
+    throw_not_implemented("create_tensor() must be defined in the test env")
 }
 
 #' Interface function to create a backend-agnostic tensor, with contents
@@ -75,22 +75,22 @@ create_tensor <- function(values, dims, ...) {
 #' @return A tensor object created by the backend, filled with sequential
 #' integers from 1 to the product of `dims`.
 create_seq_tensor <- function(dims, ...) {
-    throw_not_implemented("create_seq_tensor() must be defined in the test context")
+    throw_not_implemented("create_seq_tensor() must be defined in the test env")
 }
 
 #' Interface function to convert a tensor to a base array
 #'
-#' This function is a placeholder that should be defined in the test context.
+#' This function is a placeholder that should be defined in the test env.
 #' It is used to convert a tensor to a base R array.
 #' @param x A tensor object to be converted.
 #' @return A base R array representation of the tensor.
 as_base_array <- function(x) {
-    throw_not_implemented("as_base_array() must be defined in the test context")
+    throw_not_implemented("as_base_array() must be defined in the test env")
 }
 
 #' Interface function to flatten a tensor
 #' @param x A tensor object to be converted
 #' @return A 1 dimensional version of the tensor
 flatten <- function(x) {
-    throw_not_implemented("flatten() must be defined in the test context")
+    throw_not_implemented("flatten() must be defined in the test env")
 }
